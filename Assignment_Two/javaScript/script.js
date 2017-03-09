70a57ae6213920370c0504a6a4b12f00ff7c17c5
tom hogan <tom hogan>
2017-03-09 18:56:11 +0000

    breakpoint progress

--- Assignment_Two/javaScript/script.js
+++ Assignment_Two/javaScript/script.js
@@ -15,3 +15,24 @@ function openMenu() {
 function closeMenu() {
     document.getElementById("mainMenu").style.width = "0";
 }
+
+
+var clicked = false, clickY;
+$(document).on({
+    'mousemove': function(e) {
+        clicked && updateScrollPos(e);
+    },
+    'mousedown': function(e) {
+        clicked = true;
+        clickY = e.pageY;
+    },
+    'mouseup': function() {
+        clicked = false;
+        $('html').css('cursor', 'auto');
+    }
+});
+
+var updateScrollPos = function(e) {
+    $('html').css('cursor', 'row-resize');
+    $(window).scrollTop($(window).scrollTop() + (clickY - e.pageY));
+}
