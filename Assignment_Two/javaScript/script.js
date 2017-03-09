b1445db77769eeaa83ef978fa306185fd5ba2190
tom hogan <tom hogan>
2017-03-09 14:24:27 +0000

    homepages pretty much finished, pop out menu done.

--- Assignment_Two/javaScript/script.js
+++ Assignment_Two/javaScript/script.js
@@ -1,6 +1,17 @@
+/* Fades out the Splashscreen when first opening the website */
 $(document).ready(function(){ 
 	$('.enter').click(function () {
    	$(this).parent('#homeSplash').fadeOut(1000);
 	});			
 });
 
+
+/* Opens mainMenu */
+function openMenu() {
+    document.getElementById("mainMenu").style.width = "100%";
+}
+
+/* Closes mainMenu */
+function closeMenu() {
+    document.getElementById("mainMenu").style.width = "0";
+}
